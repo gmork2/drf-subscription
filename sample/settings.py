f534c889ae62afbd496e9cc6b9e408c860b73d23
gmork2 <gmork.02@gmail.com>
2020-11-24 18:15:51 +0100

    Added subscription middleware to settings of sample project

--- sample/settings.py
+++ sample/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'subscription.middleware.SubscriberMiddleware',
 ]
 
 ROOT_URLCONF = 'sample.urls'
