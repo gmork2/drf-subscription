0ce81bed677baa5cfaad53f732e02bf482eb3b0e
gmork2 <gmork.02@gmail.com>
2020-11-24 12:43:26 +0100

    Created resource model admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Resource
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subscription_event', 'content_object_fields', 'callback', 'content_object')
+    fieldsets = (
+        (None, {
+            'fields': ('subscription_event', 'content_object_fields', 'callback')
+        }),
+        ('Resource', {
+            'description': 'Resource form',
+            'classes': ('wide',),
+            'fields': ('content_type', 'object_pk',),  # 'content_object',
+        }),
+    )
