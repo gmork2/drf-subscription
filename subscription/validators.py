e861e6b5e92034b21d7c79938fd2a948287b630d
gmork2 <gmork.02@gmail.com>
2020-11-28 13:20:34 +0100

    Added deconstructible decorator to CallBackValidator

--- subscription/validators.py
+++ subscription/validators.py
@@ -1,9 +1,11 @@
+from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
 from django.utils.module_loading import import_string
 
 
+@deconstructible
 class CallBackValidator(object):
     message = _('Invalid path: "{}"')
     code = "invalid"
