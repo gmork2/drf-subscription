7984129326336bbc35a4694d2bd934ce5de32f21
gmork2 <gmork.02@gmail.com>
2020-12-04 19:31:23 +0100

    Added disconnect action to resource admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.utils.translation import gettext as _
 from django.core.handlers.wsgi import WSGIRequest
+from django.db.models.signals import post_save
+from django.contrib.messages import constants
 
-from .managers import SubscriptionQuerySet
+from .managers import SubscriptionQuerySet, ResourceQuerySet
 from .models import Subscription, SubscriptionLine, SubscriptionEvent, Resource
+from .signals import default_receiver
 
 
 def activate(
-        modeladmin: 'SubscriptionAdmin',
-        request: WSGIRequest,
-        queryset: SubscriptionQuerySet
+    modeladmin: 'SubscriptionAdmin',
+    request: WSGIRequest,
+    queryset: SubscriptionQuerySet
 ):
     for instance in queryset:
         instance.activate()
@@ -17,6 +20,24 @@ def activate(
 
 
 activate.short_description = "Activate a subscription"
+
+
+def disconnect(
+    modeladmin: 'SubscriptionAdmin',
+    request: WSGIRequest,
+    queryset: ResourceQuerySet
+) -> None:
+    for instance in queryset:
+        model_class = instance.content_type.model_class()
+        if not post_save.disconnect(default_receiver, sender=model_class):
+            modeladmin.message_user(
+                request,
+                _('Unable to disconnect: %s' % model_class),
+                level=constants.ERROR
+            )
+
+
+disconnect.short_description = "Disconnect signals"
 
 
 @admin.register(Subscription)
@@ -84,3 +105,4 @@ class ResourceAdmin(admin.ModelAdmin):
             'fields': ('content_type', 'object_pk', 'content_object_fields'),  # 'content_object',
         }),
     )
+    actions = (disconnect,)
