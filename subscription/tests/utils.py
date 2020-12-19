9aa480ed3bd0602f12990423f6a57a4272e38ab0
gmork2 <gmork.02@gmail.com>
2020-12-19 15:03:07 +0100

    New split_path function in tests utils

--- subscription/tests/utils.py
+++ subscription/tests/utils.py
@@ -1,7 +1,11 @@
 import logging
 from pprint import pprint
+import os
+from typing import Optional, List
 
 logger = logging.getLogger(__name__)
+
+DUMMY_DOTTED_PATH = 'subscription.tests.utils.dummy'
 
 
 def dummy(*args, **kwargs) -> None:
@@ -10,3 +14,12 @@ def dummy(*args, **kwargs) -> None:
     """
     logger.info(f'kwargs: {kwargs}')
     pprint(kwargs)
+
+
+def split_path(path: str, items: List[str]) -> Optional[List[str]]:
+    if path:
+        head, tail = os.path.split(path)
+        if tail:
+            items.insert(0, tail)
+            split_path(head, items)
+        return items
