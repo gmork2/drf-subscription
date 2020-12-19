9bed6eab9437511c1c27862510883eb3214c7ff1
gmork2 <gmork.02@gmail.com>
2020-12-19 15:35:38 +0100

    Ensure DUMMY_DOTTED_PATH is valid for dotted path tests

--- subscription/tests/test_validators.py
+++ subscription/tests/test_validators.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.module_loading import import_string
 
 from subscription.validators import ImportCallBackValidator
 from .utils import DUMMY_DOTTED_PATH
@@ -20,6 +21,11 @@ class ImportCallBackValidatorTestCase(TestCase):
         pass
 
     def test_valid_dotted_path(self):
+        try:
+            import_string(self.dotted_path)
+        except ImportError:
+            self.skipTest("Fix DUMMY_DOTTED_PATH to run this test!")
+
         self.assertIsNone(self.validator(self.dotted_path))
 
     def test_object_is_not_callable(self):
@@ -27,4 +33,3 @@ class ImportCallBackValidatorTestCase(TestCase):
 
     def test_object_is_callable(self):
         pass
-
