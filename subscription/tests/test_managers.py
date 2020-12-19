0cc15e419689a9724b07ad12206bc3d40f414259
gmork2 <gmork.02@gmail.com>
2020-12-19 15:02:11 +0100

    Created test_managers

---
+++ subscription/tests/test_managers.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from subscription.managers import (
+    SubscriptionManager, SubscriptionEventManager, SubscriptionLineManager, ResourceManager
+)
+
+
+class SubscriptionManagerTestCase(TestCase):
+    pass
+
+
+class SubscriptionEventManagerTestCase(TestCase):
+    pass
+
+
+class SubscriptionLineManagerTestCase(TestCase):
+    pass
+
+
+class ResourceManagerTestCase(TestCase):
+    pass
