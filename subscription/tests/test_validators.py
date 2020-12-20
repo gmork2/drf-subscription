3d82e8a42739873a9eca5e03246cfb1d70ef8f9e
gmork2 <gmork.02@gmail.com>
2020-12-20 13:39:36 +0100

    Tests implemented in ImportCallBackValidatorTestCase

--- subscription/tests/test_validators.py
+++ subscription/tests/test_validators.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils.module_loading import import_string
+from django.core.exceptions import ValidationError
 
 from subscription.validators import ImportCallBackValidator
 from .utils import DUMMY_DOTTED_PATH
@@ -18,18 +19,18 @@ class ImportCallBackValidatorTestCase(TestCase):
         self.assertIsNone(self.validator(''))
 
     def test_invalid_dotted_path(self):
-        pass
+        self.assertRaises(ValidationError, self.validator, f'x.{DUMMY_DOTTED_PATH}')    # ModuleNotFoundError
+        self.assertRaises(ValidationError, self.validator, 'subscription')              # ImportError
+        self.assertRaises(ValidationError, self.validator, '.')                         # ValueError
 
-    def test_valid_dotted_path(self):
+    def test_object_is_not_callable(self):
+        dotted_path = 'subscription.tests.utils.DUMMY_DOTTED_PATH'
+        self.assertRaises(ValidationError, self.validator, dotted_path)                 # TypeError
+
+    def test_object_is_callable(self):
         try:
             import_string(self.dotted_path)
         except ImportError:
             self.skipTest("Fix DUMMY_DOTTED_PATH to run this test!")
 
         self.assertIsNone(self.validator(self.dotted_path))
-
-    def test_object_is_not_callable(self):
-        pass
-
-    def test_object_is_callable(self):
-        pass
