c46269773b22d59503e640a733ef4982bc86f9de
gmork2 <gmork.02@gmail.com>
2020-12-23 01:13:47 +0100

    Add connect method to resource

--- subscription/models/resource.py
+++ subscription/models/resource.py
@@ -97,6 +97,14 @@ class Resource(BaseGenericObjectResource):
             self.subscription_event.subscription_line.subscription.active and \
             self.active
 
+    def connect(self, model_class: models.Model) -> None:
+        """
+
+        :param model_class:
+        :return:
+        """
+        self.signal(self.receiver, sender=model_class)
+
     def clean(self):
         """
 
