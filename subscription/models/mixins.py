d1347bd7245719b0cd47a1c91533e7096e61fcfd
gmork2 <gmork.02@gmail.com>
2021-01-29 19:43:22 +0100

    Added MonthlyEventMixin to mixins module

--- subscription/models/mixins.py
+++ subscription/models/mixins.py
@@ -1,4 +1,5 @@
 from typing import Generator
+from calendar import monthrange
 
 from django.db import models
 from django.core.exceptions import ValidationError
@@ -126,3 +127,29 @@ class PeriodicEventMixin(AbstractEventMixin):
 
     class Meta:
         abstract = True
+
+
+class MonthlyEventMixin(PeriodicEventMixin):
+    @property
+    def recurrence(self):
+        """
+        Returns the number of days in the current month.
+
+        :return:
+        """
+        days = monthrange(self.start.year, self.start.month)[1]
+        return timezone.timedelta(days=days)
+
+    def __iadd__(self, duration: timezone.timedelta):
+        """
+        Set the start and end date with the value of the first and last
+        day of the following month respectively.
+
+        :param duration:
+        :return:
+        """
+        self.start = self.start.replace(day=1) + duration
+        days = monthrange(self.start.year, self.start.month)[1]
+        self.end = self.start.replace(day=days)
+
+        return self
